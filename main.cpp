4a506e942889b44f17c531129fe7300225ef73a6
Tthang07 <toanthang071106@gmail.com>
2025-04-29 12:05:10 +0700

    lần 7

--- main.cpp
+++ main.cpp
@@ -12,11 +12,18 @@
 #include <fstream>
 using namespace std;
 
+enum GameMode {
+    MENU,
+    SURVIVAL,
+    BOSS,
+    EXIT
+};
+
 const int SCREEN_WIDTH = 1200;
 const int SCREEN_HEIGHT = 800;
 const int PLAYER_WIDTH = 80;
 const int PLAYER_HEIGHT = 140;
-const int ENEMY_WIDTH = 100;
+const int ENEMY_WIDTH = 85;
 const int ENEMY_HEIGHT = 85;
 const int BULLET_WIDTH = 40;
 const int BULLET_HEIGHT = 70;
@@ -51,9 +58,7 @@ struct Player {
 };
 
 int enemyWaveCount = 0;
-int enemySpeed = 3;
-int enemyBulletSpeed = 6;
-int enemySpawnRate = 60;
+int highScore = 0;
 
 void spawnEnemyBullet(GameObject& enemy) {
     GameObject bullet = { enemy.x + enemy.w / 2 - 10, enemy.y + enemy.h, 20, 50, true };
@@ -98,6 +103,40 @@ void renderText(SDL_Renderer* renderer, TTF_Font* font, const string& text, int 
     SDL_DestroyTexture(texture);
 }
 
+void renderMenu(SDL_Renderer* renderer, TTF_Font* font, int selectedOption, int highScore) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    string options[3] = { "1. Survival", "2. Campaign", "3. Exit" };
+
+    renderText(renderer, font, "Hight score: " + to_string(highScore), SCREEN_WIDTH/2 - 80, 150);
+
+    for (int i = 0; i < 3; ++i) {
+        SDL_Color color = (i == selectedOption) ? SDL_Color{255, 255, 0} : SDL_Color{255, 255, 255};
+        SDL_Surface* surface = TTF_RenderText_Solid(font, options[i].c_str(), color);
+        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_Rect rect = { SCREEN_WIDTH/2 - surface->w/2, 250 + i * 100, surface->w, surface->h };
+        SDL_FreeSurface(surface);
+        SDL_RenderCopy(renderer, texture, NULL, &rect);
+        SDL_DestroyTexture(texture);
+    }
+
+    SDL_RenderPresent(renderer);
+}
+
+void resetGame(Player& player, vector<GameObject>& bullets,
+              vector<GameObject>& enemies, vector<GameObject>& enemyBullets,
+              vector<Explosion>& explosions, int& enemyWaveCount) {
+    player = { SCREEN_WIDTH / 2 - PLAYER_WIDTH / 2, SCREEN_HEIGHT - PLAYER_HEIGHT - 10 };
+    player.lives = 3;
+    player.score = 0;
+    bullets.clear();
+    enemies.clear();
+    enemyBullets.clear();
+    explosions.clear();
+    enemyWaveCount = 0;
+}
+
 int main() {
     srand(time(0));
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
@@ -105,65 +144,106 @@ int main() {
     TTF_Init();
     Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
 
+    // Load sound
     Mix_Chunk* soundStart = Mix_LoadWAV("fight.mp3");
     Mix_Chunk* soundExplode = Mix_LoadWAV("địch nổ.mp3");
     Mix_Chunk* soundGameOver = Mix_LoadWAV("over.mp3");
     Mix_Chunk* soundHit = Mix_LoadWAV("trúng đạn.mp3");
     Mix_Chunk* soundShoot = Mix_LoadWAV("voice đạn.mp3");
 
-    Mix_Music* bgMusic = Mix_LoadMUS("nhạc nền.mp3");
-
     SDL_Window* window = SDL_CreateWindow("Space Shooter", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    TTF_Font* font = TTF_OpenFont("PixelFont.ttf", 28);
+    TTF_Font* font = TTF_OpenFont("PixelFont.ttf", 25);
     if (!font) {
-        cout << "Không thể tải font: " << TTF_GetError() << endl;
+        cout << "Khong the tai font: " << TTF_GetError() << endl;
         return -1;
     }
 
-    SDL_Surface* surface = IMG_Load("tàu 1.png");
-    if (!surface) {
-        cout << "Không thể load ảnh máy bay: " << IMG_GetError() << endl;
-        return -1;
-    }
-    SDL_Texture* playerTexture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
-
-    SDL_Texture* bulletTexture = IMG_LoadTexture(renderer, "đạn 4.png");
+    // Load textures
+    SDL_Texture* playerTexture = IMG_LoadTexture(renderer, "tàu.png");
+    SDL_Texture* bulletTexture = IMG_LoadTexture(renderer, "đạn.png");
     SDL_Texture* enemyTexture = IMG_LoadTexture(renderer, "địch.png");
     SDL_Texture* backgroundTexture = IMG_LoadTexture(renderer, "nền.png");
     SDL_Texture* enemyBulletTexture = IMG_LoadTexture(renderer, "đạn địch.png");
-    SDL_Texture* lifeTexture = IMG_LoadTexture(renderer, "tàu 1.png");
+    SDL_Texture* lifeTexture = IMG_LoadTexture(renderer, "tàu.png");
     SDL_Texture* explosionTexture = IMG_LoadTexture(renderer, "địch nổ.png");
-
     SDL_Texture* startTexture = IMG_LoadTexture(renderer, "fight.png");
     SDL_Texture* gameOverTexture = IMG_LoadTexture(renderer, "over.jpg");
 
-    Player player = { SCREEN_WIDTH / 2 - PLAYER_WIDTH / 2, SCREEN_HEIGHT - PLAYER_HEIGHT - 10 };
-
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, startTexture, NULL, NULL);
-    SDL_RenderPresent(renderer);
-    Mix_PlayChannel(-1, soundStart, 0);
-    SDL_Delay(2000);
-
-    if (bgMusic) {
-        Mix_PlayMusic(bgMusic, -1);
+    // Load high score
+    ifstream in("highscore.txt");
+    if (in) {
+        in >> highScore;
+        in.close();
     }
 
+    Player player = { SCREEN_WIDTH / 2 - PLAYER_WIDTH / 2, SCREEN_HEIGHT - PLAYER_HEIGHT - 10 };
+    GameMode gameMode = MENU;
+    int selectedOption = 0;
     bool running = true;
     SDL_Event event;
     int enemySpawnCounter = 0;
     int enemyShootCounter = 0;
     int bulletCooldown = 0;
-    int shakeTimer = 0;
 
     while (running) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) running = false;
+
+            if (gameMode == MENU) {
+                if (event.type == SDL_KEYDOWN) {
+                    if (event.key.keysym.sym == SDLK_DOWN) {
+                        selectedOption = (selectedOption + 1) % 3;
+                    }
+                    if (event.key.keysym.sym == SDLK_UP) {
+                        selectedOption = (selectedOption + 2) % 3;
+                    }
+                    if (event.key.keysym.sym == SDLK_RETURN) {
+                        if (selectedOption == 0) {
+                            gameMode = SURVIVAL;
+                            SDL_RenderClear(renderer);
+                            SDL_RenderCopy(renderer, startTexture, NULL, NULL);
+                            SDL_RenderPresent(renderer);
+                            Mix_PlayChannel(-1, soundStart, 0);
+                            SDL_Delay(2000);
+                            resetGame(player, bullets, enemies, enemyBullets, explosions, enemyWaveCount);
+                        } else if (selectedOption == 1) {
+                            gameMode = BOSS;
+                            SDL_RenderClear(renderer);
+                            SDL_RenderCopy(renderer, startTexture, NULL, NULL);
+                            SDL_RenderPresent(renderer);
+                            Mix_PlayChannel(-1, soundStart, 0);
+                            SDL_Delay(2000);
+                            resetGame(player, bullets, enemies, enemyBullets, explosions, enemyWaveCount);
+                        } else if (selectedOption == 2) {
+                            running = false;
+                        }
+                    }
+                }
+            }
         }
 
+        if (gameMode == MENU) {
+            renderMenu(renderer, font, selectedOption, highScore);
+            SDL_Delay(16);
+            continue;
+        }
+
+        if (gameMode == BOSS) {
+            SDL_RenderClear(renderer);
+            renderText(renderer, font, "CHE DO BOSS - DANG PHAT TRIEN!", SCREEN_WIDTH/2 - 200, SCREEN_HEIGHT/2);
+            renderText(renderer, font, "NHAN ESC DE VE MENU", SCREEN_WIDTH/2 - 150, SCREEN_HEIGHT/2 + 50);
+            SDL_RenderPresent(renderer);
+
+            const Uint8* keys = SDL_GetKeyboardState(NULL);
+            if (keys[SDL_SCANCODE_ESCAPE]) {
+                gameMode = MENU;
+            }
+            continue;
+        }
+
+        // Game logic (SURVIVAL mode)
         const Uint8* keystate = SDL_GetKeyboardState(NULL);
         if (keystate[SDL_SCANCODE_LEFT]) player.moveLeft();
         if (keystate[SDL_SCANCODE_RIGHT]) player.moveRight();
@@ -189,7 +269,7 @@ int main() {
             }
         }
 
-        if (++enemySpawnCounter > enemySpawnRate) {
+        if (++enemySpawnCounter > 60) {
             spawnEnemyWave();
             enemySpawnCounter = 0;
         }
@@ -205,7 +285,7 @@ int main() {
 
         for (auto& enemy : enemies) {
             if (enemy.active) {
-                enemy.y += enemySpeed;
+                enemy.y += 3;
                 if (enemy.y > SCREEN_HEIGHT) enemy.active = false;
             }
         }
@@ -213,13 +293,14 @@ int main() {
         for (auto& bullet : bullets) {
             if (bullet.active) {
                 for (auto& enemy : enemies) {
-                    if (enemy.active && bullet.x < enemy.x + enemy.w && bullet.x + bullet.w > enemy.x && bullet.y < enemy.y + enemy.h && bullet.y + bullet.h > enemy.y) {
+                    if (enemy.active &&
+                        bullet.x < enemy.x + enemy.w && bullet.x + bullet.w > enemy.x &&
+                        bullet.y < enemy.y + enemy.h && bullet.y + bullet.h > enemy.y) {
                         explosions.push_back({ enemy.x, enemy.y });
                         enemy.active = false;
                         bullet.active = false;
                         player.score += 10;
                         Mix_PlayChannel(-1, soundExplode, 0);
-                        shakeTimer = 10;
                     }
                 }
             }
@@ -227,7 +308,7 @@ int main() {
 
         for (auto& eBullet : enemyBullets) {
             if (eBullet.active) {
-                eBullet.y += enemyBulletSpeed;
+                eBullet.y += 6;
                 if (eBullet.y > SCREEN_HEIGHT) eBullet.active = false;
 
                 SDL_Rect bRect = { eBullet.x, eBullet.y, eBullet.w, eBullet.h };
@@ -238,48 +319,54 @@ int main() {
                     player.invincible = true;
                     player.invincibleTimer = 90;
                     Mix_PlayChannel(-1, soundHit, 0);
-                    shakeTimer = 10;
+
                     if (player.lives <= 0) {
+                        // Update high score
+                        if (player.score > highScore) {
+                            highScore = player.score;
+                            ofstream out("highscore.txt");
+                            out << highScore;
+                            out.close();
+                        }
+
+                        // Show game over screen
                         SDL_RenderClear(renderer);
                         SDL_RenderCopy(renderer, gameOverTexture, NULL, NULL);
+                        renderText(renderer, font, "DIEM SO: " + to_string(player.score), SCREEN_WIDTH/2 - 100, SCREEN_HEIGHT/2 + 50);
+                        renderText(renderer, font, "NHAN PHIM BAT KY DE VE MENU", SCREEN_WIDTH/2 - 150, SCREEN_HEIGHT/2 + 100);
                         SDL_RenderPresent(renderer);
                         Mix_PlayChannel(-1, soundGameOver, 0);
-                        SDL_Delay(3000);
-                        running = false;
+
+                        // Wait for any key press
+                        bool waiting = true;
+                        while (waiting) {
+                            while (SDL_PollEvent(&event)) {
+                                if (event.type == SDL_QUIT) {
+                                    running = false;
+                                    waiting = false;
+                                }
+                                if (event.type == SDL_KEYDOWN || event.type == SDL_MOUSEBUTTONDOWN) {
+                                    waiting = false;
+                                }
+                            }
+                            SDL_Delay(16);
+                        }
+
+                        // Return to menu
+                        gameMode = MENU;
+                        resetGame(player, bullets, enemies, enemyBullets, explosions, enemyWaveCount);
                     }
                 }
             }
         }
 
+        // Clean up inactive objects
         bullets.erase(remove_if(bullets.begin(), bullets.end(), [](const GameObject& b) { return !b.active; }), bullets.end());
         enemies.erase(remove_if(enemies.begin(), enemies.end(), [](const GameObject& e) { return !e.active; }), enemies.end());
         enemyBullets.erase(remove_if(enemyBullets.begin(), enemyBullets.end(), [](const GameObject& b) { return !b.active; }), enemyBullets.end());
+        explosions.erase(remove_if(explosions.begin(), explosions.end(), [](const Explosion& e) { return e.frame > 15; }), explosions.end());
 
-        if (player.score >= 500) {
-            enemySpeed = 6;
-            enemyBulletSpeed = 9;
-            enemySpawnRate = 40;
-        } else if (player.score >= 400) {
-            enemySpeed = 5;
-            enemyBulletSpeed = 8;
-            enemySpawnRate = 45;
-        } else if (player.score >= 200) {
-            enemySpeed = 4;
-            enemyBulletSpeed = 7;
-            enemySpawnRate = 50;
-        }
-
-        int offsetX = 0, offsetY = 0;
-        if (shakeTimer > 0) {
-            offsetX = (rand() % 5) - 2;
-            offsetY = (rand() % 5) - 2;
-            shakeTimer--;
-        }
-
-        SDL_Rect camera = { offsetX, offsetY, SCREEN_WIDTH, SCREEN_HEIGHT };
-
-        SDL_RenderSetViewport(renderer, &camera);
-
+        // Rendering
         SDL_RenderCopy(renderer, backgroundTexture, NULL, NULL);
 
         if (player.invincible && player.invincibleTimer > 80) {
@@ -318,27 +405,19 @@ int main() {
             SDL_RenderCopy(renderer, explosionTexture, NULL, &rect);
             explosions[i].frame++;
         }
-        explosions.erase(remove_if(explosions.begin(), explosions.end(), [](const Explosion& e) {
-            return e.frame > 15;
-        }), explosions.end());
 
         renderScore(renderer, lifeTexture, player.lives, player.score);
-        renderText(renderer, font, "Score: " + to_string(player.score), 950, 10);
-
+        renderText(renderer, font, "Diem: " + to_string(player.score), 950, 10);
         SDL_RenderPresent(renderer);
         SDL_Delay(16);
     }
 
-    ofstream out("score.txt");
-    out << "Score: " << player.score << endl;
-    out.close();
-
+    // Clean up
     Mix_FreeChunk(soundStart);
     Mix_FreeChunk(soundExplode);
     Mix_FreeChunk(soundGameOver);
     Mix_FreeChunk(soundHit);
     Mix_FreeChunk(soundShoot);
-    Mix_FreeMusic(bgMusic);
     Mix_CloseAudio();
 
     TTF_CloseFont(font);
